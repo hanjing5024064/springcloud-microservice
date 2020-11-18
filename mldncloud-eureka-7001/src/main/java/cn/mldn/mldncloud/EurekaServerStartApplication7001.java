2f92ffd2bf3ad77d03652126d97a839d4c980bdd
zhangjun <5024064@qq.com>
2020-11-18 23:02:29 +0800

    8.2 定义Eureka服务端，准备为微服务提供注册服务

---
+++ mldncloud-eureka-7001/src/main/java/cn/mldn/mldncloud/EurekaServerStartApplication7001.java
@@ -0,0 +1,14 @@
+package cn.mldn.mldncloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer					// 启用Eureka服务
+public class EurekaServerStartApplication7001 {
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerStartApplication7001.class, args);
+	}
+
+}
