d35a4f0bacd71c7aa09febb1f54ce85232bc91a6
zhangjun <5024064@qq.com>
2020-11-18 23:09:09 +0800

    8.3 向Eureka中注册微服务：pom中添加依赖，yml中添加配置，启动类中添加注解@EnableEurekaClient http://eureka-7001.com:7001

--- mldncloud-dept-service-8001/src/main/java/cn/mldn/mldncloud/StartDeptServiceApplication8001.java
+++ mldncloud-dept-service-8001/src/main/java/cn/mldn/mldncloud/StartDeptServiceApplication8001.java
@@ -2,8 +2,10 @@ package cn.mldn.mldncloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication
+@EnableEurekaClient					// 启用Eureka客户端
 @EnableJpaRepositories(basePackages="cn.mldn.mldncloud.dao")
 public class StartDeptServiceApplication8001 {
 	public static void main(String[] args) {
