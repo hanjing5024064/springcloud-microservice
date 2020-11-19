8904e3113d289ceb1402ef011f47d661afaefc35
zhangjun <5024064@qq.com>
2020-11-19 20:50:12 +0800

    9.2.2 Feign将Restful服务的信息转换为接口，使得调用微服务如同调用本地接口一样，过程中仍然需要JSON（或者XML/文本）数据传输，为了提高效率，往往要配置数据压缩。 http://localhost/consumer/dept/list 启动Eureka->微服务8001/8002/8003->启动feign消费者

--- mldncloud-api/src/main/java/cn/mldn/mldncloud/service/config/FeignClientConfig.java
+++ mldncloud-api/src/main/java/cn/mldn/mldncloud/service/config/FeignClientConfig.java
@@ -7,6 +7,12 @@ import feign.auth.BasicAuthRequestInterceptor;
 
 @Configuration
 public class FeignClientConfig {
+	
+	@Bean
+	public feign.Logger.Level getFeignLoggerLevel() {
+		return feign.Logger.Level.FULL ;
+	}
+	
 	@Bean
 	public BasicAuthRequestInterceptor getBasicAuthRequestInterceptor() {
 		return new BasicAuthRequestInterceptor("mldnjava", "hello");
