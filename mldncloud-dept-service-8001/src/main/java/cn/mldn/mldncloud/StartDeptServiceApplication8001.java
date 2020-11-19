31ef183929a6712d76b058ecad636e982610327f
zhangjun <5024064@qq.com>
2020-11-19 21:34:22 +0800

    9.3.1 微服务8001启用熔断机制Hystrix

--- mldncloud-dept-service-8001/src/main/java/cn/mldn/mldncloud/StartDeptServiceApplication8001.java
+++ mldncloud-dept-service-8001/src/main/java/cn/mldn/mldncloud/StartDeptServiceApplication8001.java
@@ -2,12 +2,14 @@ package cn.mldn.mldncloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication
 @EnableEurekaClient					// 启用Eureka客户端
 @EnableDiscoveryClient
+@EnableCircuitBreaker				// 启用熔断机制
 @EnableJpaRepositories(basePackages="cn.mldn.mldncloud.dao")
 public class StartDeptServiceApplication8001 {
 	public static void main(String[] args) {
