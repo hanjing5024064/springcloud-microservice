34a5b7214fa7b4b11d072137ae2d710d897d378e
zhangjun <5024064@qq.com>
2020-11-19 23:45:03 +0800

    9.3.3 HystrixDashboard 监控微服务状态，微服务引入依赖hystrix-dashboad actuator, 需要监控的方法添加@HystrixCommand注解，启动类加载Hystrix 启动微服务8001，服务注册7001，微服务消费端80，监控面板9001 http://dashboard.com:9001/hystrix 监控输入：http://mldnjava:hello@dept-8001.com/8001/hystrix.stream http://dashboard.com:9001/hystrix/monitor?stream=http%3A%2F%2Fmldnjava%3Ahello%40dept-8001.com%3A8001%2Fhystrix.stream

--- mldncloud-dept-service-8001/src/main/java/cn/mldn/mldncloud/StartDeptServiceApplication8001.java
+++ mldncloud-dept-service-8001/src/main/java/cn/mldn/mldncloud/StartDeptServiceApplication8001.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication
 @EnableEurekaClient					// 启用Eureka客户端
 @EnableDiscoveryClient
 @EnableCircuitBreaker				// 启用熔断机制
+@EnableHystrix						// 启用Hystrix支持
 @EnableJpaRepositories(basePackages="cn.mldn.mldncloud.dao")
 public class StartDeptServiceApplication8001 {
 	public static void main(String[] args) {
--- mldncloud-dept-service-8001/src/main/java/cn/mldn/mldncloud/rest/DeptRest.java
+++ mldncloud-dept-service-8001/src/main/java/cn/mldn/mldncloud/rest/DeptRest.java
@@ -1,5 +1,7 @@
 package cn.mldn.mldncloud.rest;
 
+import java.util.ArrayList;
+
 import javax.annotation.Resource;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 import cn.mldn.mldncloud.dto.DeptDTO;
 import cn.mldn.mldncloud.service.IDeptService;
@@ -22,21 +26,31 @@ public class DeptRest {
 	@Resource
 	private DiscoveryClient client ;	// 进行Eureka的发现服务
 	@RequestMapping("/dept/discover")
+	@HystrixCommand						// Hystrix监控注解
 	public Object discover() {	// 直接返回发现服务信息
 		return this.client ;
 	}
-
 	
 	@PostMapping("/dept/add")
-	public Object get(@RequestBody DeptDTO dept) {
+	@HystrixCommand					// Hystrix监控注解
+	public Object add(@RequestBody DeptDTO dept) {
 		return this.deptService.add(dept)  ;	// 增加部门信息
 	}
 	@GetMapping("/dept/get/{deptno}")
+	@HystrixCommand					// Hystrix监控注解
 	public Object get(@PathVariable("deptno") long deptno) {
+		if (deptno == 0L) {	// 抛出异常
+			throw new RuntimeException("Invalidate Dept Info") ;
+		}
 		return this.deptService.get(deptno)  ;	// 查询部门信息
 	}
 	@GetMapping("/dept/list")
+	@HystrixCommand(fallbackMethod="listFallback")// Hystrix监控注解,当list方法执行有问题时会自动调用listFallback()方法进行失败处理
 	public Object list() {
 		return this.deptService.list() ;		// 部门信息列表
+	}
+	
+	public Object listFallback() {
+		return new ArrayList<DeptDTO>();
 	}
 }
