d2934d57cfb88371850c6828ad1729128698e501
zhangjun <5024064@qq.com>
2020-11-23 21:18:14 +0800

    12.3 SpringCloudSleuth数据采集微服务，业务微服务产生消息到RabbitMQ，Sleuth作为消息消费者，将收到的消息保存到数据库 启动：eureka-7001，dept-service-8001/8002/8003，zuul-9501, consumer-feign, sleuth-8601 1. 访问业务消费者 http://localhost/consumer/dept/get?deptno=2 http://localhost/consumer/dept/list 2. 访问RabbitMQ查看消息队列 http://localhost:15672/ guest/guest 3. 访问Sleush查看服务调用队列 http://zipkin.com:8601/zipkin/?serviceName=all&spanName=all&startTs=1605974400000&endTs=1606224480000&minDuration=&limit=10&annotationQuery=&sortOrder=duration-desc admin/hello 4. 访问数据库zipkin 查看持久化的消息队列

--- mldncloud-sleuth-8601/src/main/java/cn/mldn/mldncloud/StartSleuthApplication8601.java
+++ mldncloud-sleuth-8601/src/main/java/cn/mldn/mldncloud/StartSleuthApplication8601.java
@@ -2,10 +2,9 @@ package cn.mldn.mldncloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import zipkin.server.EnableZipkinServer;
+import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
 @SpringBootApplication
-@EnableZipkinServer					// 启用Zipkin服务
+@EnableZipkinStreamServer	
 public class StartSleuthApplication8601 {
 	public static void main(String[] args) {
 		SpringApplication.run(StartSleuthApplication8601.class, args);
